0f45f09d70584603fdf2c31133f547727f239351
ralhassan <ralhassan@site.sa>
2020-12-19 18:49:05 +0300

    adding models

---
+++ models.py
@@ -0,0 +1,122 @@
+
+#----------------------------------------------------------------------------#
+# Models section
+#----------------------------------------------------------------------------#
+
+import json
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, Date
+from flask_migrate import Migrate
+from sqlalchemy.orm import backref
+
+database_name = "castingAgency"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+class Movie(db.Model):
+    """
+        Movie model
+
+        - Fields
+            1. id
+            2. title
+            3. release_date
+
+        - Fucntions
+            1.insert()
+            2. update()
+            3. delete()
+            4. format()
+
+    """
+    __tablename__ = 'Movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    release_date = Column(Date)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'title': self.title,
+        'release_date': self.release_date,
+        }
+
+
+
+class Actor(db.Model):
+    """
+        Actor model
+
+        - Fields
+            1. id
+            2. name
+            3. age
+            4. gender
+
+        - Fucntions
+            1.insert()
+            2. update()
+            3. delete()
+            4. format()
+
+    """
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(120))
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'age': self.age,
+        'gender': self.gender,
+        }
+
